4b1762c76fa1eec424ec1129e5622745a9d81d30
Maria Smith <maria.t.smith023@gmail.com>
2026-02-26 15:00:34 -0500

    add sign up button functionality, error messages, pass masking

--- BasicSignUpProfilePages/MainPage.xaml.cs
+++ BasicSignUpProfilePages/MainPage.xaml.cs
@@ -6,5 +6,23 @@
         {
             InitializeComponent();
         }
+
+        private void btnSignUp_Clicked(object sender, EventArgs e)
+        {
+            lblErrorMessage.Text = "";
+
+            if (string.IsNullOrWhiteSpace(txtUsername.Text) 
+                || string.IsNullOrWhiteSpace(txtEmail.Text)
+                || string.IsNullOrWhiteSpace(txtPassword.Text)
+                || string.IsNullOrWhiteSpace(txtConfirmPassword.Text))
+            {
+                lblErrorMessage.Text = "All fields must be filled.";
+            }
+
+            if (txtPassword.Text != txtConfirmPassword.Text)
+            {
+                lblErrorMessage.Text = "Passwords must match.";
+            }
+        }
     }
 }
