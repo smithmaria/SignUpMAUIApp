7dead1e25733c8d134ec398bf4514d4de1f2a4db
Maria Smith <maria.t.smith023@gmail.com>
2026-02-26 14:36:28 -0500

    initial commit

---
+++ BasicSignUpProfilePages/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BasicSignUpProfilePages")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BasicSignUpProfilePages.Pages")]
---
+++ BasicSignUpProfilePages/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BasicSignUpProfilePages
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
